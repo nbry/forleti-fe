dae7b83b0ae1664abc5d5e096e140fb84976c479
nbry <nwbryant@umich.edu>
2021-01-11 11:39:13 -0800

    Set App file as a style wrapper for landing page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { CssBaseline } from "@material-ui/core";
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <CssBaseline />
+    </Wrapper>
   );
 }
 
 export default App;
+
+//STYLES:
+const backgroundURL = `${process.env.PUBLIC_URL + "/assets/typewriter-bg.jpg"}`;
+
+const Wrapper = styled.div`
+  min-height: 100vh;
+  background-image: url(${backgroundURL});
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
