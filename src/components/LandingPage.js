4dd965a6fe8f6c850419b4925b50e50798a68f03
nbry <nwbryant@umich.edu>
2021-01-11 14:47:28 -0800

    Add loading animation for landing page title

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import Header from "./Header";
+import { Collapse, IconButton } from "@material-ui/core";
 
 function LandingPage() {
+  const [checked, setChecked] = useState(false);
+  useEffect(() => {
+    setChecked(true);
+  });
+
   return (
-    <Wrapper>
-      <Header />
-    </Wrapper>
+    <BackgroundContainer>
+      <Wrapper>
+        <Header />
+        <Collapse
+          in={checked}
+          {...(checked ? { timeout: 1000 } : {})}
+          collapsedHeight={50}
+        >
+          <InnerWrapper>
+            <Title>
+              Welcome to <br /> <span>Forleti</span>
+            </Title>
+            <IconButton>
+              <StyledExpandMoreIcon />
+            </IconButton>
+          </InnerWrapper>
+        </Collapse>
+      </Wrapper>
+    </BackgroundContainer>
   );
 }
 
@@ -15,9 +38,30 @@ export default LandingPage;
 //STYLES:
 const backgroundURL = `${process.env.PUBLIC_URL + "/assets/typewriter-bg.jpg"}`;
 
-const Wrapper = styled.div`
+const BackgroundContainer = styled.div`
   min-height: 100vh;
   background-image: url(${backgroundURL});
   background-repeat: no-repeat;
   background-size: cover;
+`;
+
+const InnerWrapper = styled.div`
+  text-align: center;
+`;
+
+const StyledExpandMoreIcon = styled(ExpandMoreIcon)`
+  color: rgb(67, 238, 164);
+  font-size: 5rem;
+`;
+
+const Title = styled.h2`
+  color: white;
+  font-size: 4.5rem;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
 `;
