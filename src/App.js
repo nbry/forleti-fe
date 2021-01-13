e030a8b96b386e41ba6db2bfae094e07f94cc12f
nbry <nwbryant@umich.edu>
2021-01-13 11:45:12 -0800

    Set up LoginContext

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import LoginContext from "./components/LoginContext";
 import Routes from "./components/Routes";
 import Header from "./components/Header/Header";
 
 function App() {
+  // [LoginContext] CHECK IF USER IS LOGGED IN
+  const [loggedIn, setLoggedIn] = useState(
+    localStorage.getItem("_token") !== null
+  );
+
+  const logOut = () => {
+    localStorage.clear();
+    setLoggedIn(false);
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem("_token")) {
+      setLoggedIn(true);
+    }
+  }, [loggedIn]);
+  // [END LoginCOntext]
+
   return (
     <>
-      <Header />
-      <Routes />
+      <LoginContext.Provider value={{ loggedIn, setLoggedIn, logOut }}>
+        <Header />
+        <Routes />
+      </LoginContext.Provider>
     </>
   );
 }
