55c67f6d70c05b3dbc8368bbee1f2e4431b54979
nbry <nwbryant@umich.edu>
2021-01-14 09:44:00 -0800

    Add padding and appropriate font sizes for PostCard

--- src/components/ProfilePage/PostCard/PostCard.js
+++ src/components/ProfilePage/PostCard/PostCard.js
@@ -1,4 +1,4 @@
-import { Paper } from "@material-ui/core";
+import { Box, Paper } from "@material-ui/core";
 import React from "react";
 import styled from "styled-components";
 
@@ -6,21 +6,24 @@ function PostCard() {
   return (
     <StyledPaper elevation={5}>
       {/* Implement Dynamic card generation later */}
-      <h3>Testing Post Title</h3>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-        tortor enim, adipiscing in semper a, pellentesque quis sem. Vivamus eu
-        odio at tellus sollicitudin viverra. Sed dignissim molestie imperdiet.
-        Nulla sit amet tortor eu nisl vestibulum dignissim. Donec scelerisque
-        vulputate tortor, non dapibus massa molestie at. Pellentesque habitant
-        morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-        Etiam a mauris sit amet augue suscipit dapibus non in lectus. Nunc
-        mattis eros ligula, ut tincidunt sapien. Nulla eget velit ipsum.
-        Suspendisse potenti. Suspendisse et sem at sem sagittis lobortis.
-        Quisque lacinia, nisl in elementum aliquet, arcu neque rhoncus tellus,
-        at ornare mi enim sit amet sem. Phasellus a nisi vel ipsum laoreet
-        mollis.
-      </p>
+
+      <PostCardTitle>Testing Post Title</PostCardTitle>
+      <div className="postContent">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+          tortor enim, adipiscing in semper a, pellentesque quis sem. Vivamus eu
+          odio at tellus sollicitudin viverra. Sed dignissim molestie imperdiet.
+          Nulla sit amet tortor eu nisl vestibulum dignissim. Donec scelerisque
+          vulputate tortor, non dapibus massa molestie at. Pellentesque habitant
+          morbi tristique senectus et netus et malesuada fames ac turpis
+          egestas. Etiam a mauris sit amet augue suscipit dapibus non in lectus.
+          Nunc mattis eros ligula, ut tincidunt sapien. Nulla eget velit ipsum.
+          Suspendisse potenti. Suspendisse et sem at sem sagittis lobortis.
+          Quisque lacinia, nisl in elementum aliquet, arcu neque rhoncus tellus,
+          at ornare mi enim sit amet sem. Phasellus a nisi vel ipsum laoreet
+          mollis.
+        </p>
+      </div>
     </StyledPaper>
   );
 }
@@ -28,7 +31,13 @@ function PostCard() {
 export default PostCard;
 
 // STYLES:
+
+const PostCardTitle = styled.h3`
+  font-size: 1.8em;
+  margin: 0px;
+`;
+
 const StyledPaper = styled(Paper)`
-  min-height: 300px;
+  padding: 20px;
   width: 95%;
 `;
