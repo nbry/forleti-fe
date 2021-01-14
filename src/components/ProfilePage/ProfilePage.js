4fc66879c8810b0db589a9256fddc6efbd6b1586
nbry <nwbryant@umich.edu>
2021-01-14 11:12:41 -0800

    Implement Fade animation for ProfilePage

--- src/components/ProfilePage/ProfilePage.js
+++ src/components/ProfilePage/ProfilePage.js
@@ -1,23 +1,34 @@
-import { Grid, Paper } from "@material-ui/core";
-import React from "react";
+import { Grid, Fade } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import ProfilePageHeader from "./ProfilePageHeader";
 import PostCard from "./PostCard/PostCard";
 import myColors from "../../static/colors";
 
 function ProfilePage() {
+  // State for loading profile fade animation:
+  const [profileLoaded, setProfileLoaded] = useState(false);
+
+  useEffect(() => {
+    setProfileLoaded(true);
+  }, []);
+
   return (
     <BackgroundWrapper container direction="row" justify="center">
       <Board>
-        <ProfilePageHeader />
-        {/* Implement a loop later */}
-        <Grid container direction="row" justify="center">
-          {/* Implement a loop later */}
-          <PostCard />
-          <PostCard />
-          <PostCard />
-          <PostCard />
-        </Grid>
+        <Fade in={profileLoaded} {...(profileLoaded ? { timeout: 1500 } : {})}>
+          <div>
+            <ProfilePageHeader />
+            {/* Implement a loop later */}
+            <Grid container direction="row" justify="center">
+              {/* Implement a loop later */}
+              <PostCard />
+              <PostCard />
+              <PostCard />
+              <PostCard />
+            </Grid>
+          </div>
+        </Fade>
       </Board>
     </BackgroundWrapper>
   );
