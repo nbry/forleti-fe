bfac6fb256d82b65c5127f9e5b346ee3e202f1c0
nbry <nwbryant@umich.edu>
2021-01-14 11:35:41 -0800

    Separate AccountMenu from HeaderMenuList

---
+++ src/components/Header/AccountMenu/AccountMenu.js
@@ -0,0 +1,119 @@
+import React, { useContext } from "react";
+import {
+  ClickAwayListener,
+  Grow,
+  Paper,
+  Popper,
+  MenuItem,
+  MenuList,
+  IconButton,
+} from "@material-ui/core";
+import PersonIcon from "@material-ui/icons/Person";
+import styled, { css } from "styled-components";
+import LoginContext from "../../LoginContext";
+import myColors from "../../../static/colors";
+
+function AccountMenu() {
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+    setOpen(false);
+  };
+
+  function handleListKeyDown(event) {
+    if (event.key === "Tab") {
+      event.preventDefault();
+      setOpen(false);
+    }
+  }
+
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
+
+  // Log Out Functionality
+  const { logOut } = useContext(LoginContext);
+  function handleLogOut(event) {
+    handleClose(event);
+    logOut();
+  }
+
+  return (
+    <>
+      <IconButton
+        ref={anchorRef}
+        aria-controls={open ? "menu-list-grow" : undefined}
+        aria-haspopup="true"
+        aria-label="menu"
+        onClick={handleToggle}
+      >
+        <SyledPersonIcon />
+      </IconButton>
+
+      <Popper
+        open={open}
+        anchorEl={anchorRef.current}
+        role={undefined}
+        transition
+        disablePortal
+      >
+        {({ TransitionProps, placement }) => (
+          <Grow
+            {...TransitionProps}
+            style={{
+              transformOrigin:
+                placement === "bottom" ? "center top" : "center bottom",
+            }}
+          >
+            <Paper>
+              <ClickAwayListener onClickAway={handleClose}>
+                <MenuList
+                  autoFocusItem={open}
+                  id="menu-list-grow"
+                  onKeyDown={handleListKeyDown}
+                >
+                  <MenuItem onClick={handleClose}>Profile</MenuItem>
+                  <MenuItem onClick={handleClose}>Settings</MenuItem>
+                  <MenuItem onClick={handleLogOut}>Logout</MenuItem>
+                </MenuList>
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
+    </>
+  );
+}
+
+export default AccountMenu;
+
+// STYLES:
+const sharedIconStyles = css`
+  font-size: 1.5rem;
+  color: white;
+  :hover {
+    transition: 50ms;
+    color: ${myColors.seaGreen};
+  }
+`;
+
+
+const SyledPersonIcon = styled(PersonIcon)`
+  ${sharedIconStyles}
+`;
+
+
--- src/components/Header/HeaderMenuList.js
+++ src/components/Header/HeaderMenuList.js
@@ -1,108 +1,22 @@
-import React, { useContext } from "react";
-import {
-  ClickAwayListener,
-  Grow,
-  Paper,
-  Popper,
-  MenuItem,
-  MenuList,
-  IconButton,
-} from "@material-ui/core";
+import React from "react";
+import { IconButton } from "@material-ui/core";
 import AddBoxIcon from "@material-ui/icons/AddBox";
-import PersonIcon from "@material-ui/icons/Person";
 import HomeIcon from "@material-ui/icons/Home";
 import styled, { css } from "styled-components";
-import LoginContext from "../LoginContext";
+import AccountMenu from "./AccountMenu/AccountMenu";
 import myColors from "../../static/colors";
 
 function HeaderMenuList() {
-  const [open, setOpen] = React.useState(false);
-  const anchorRef = React.useRef(null);
-
-  const handleToggle = () => {
-    setOpen((prevOpen) => !prevOpen);
-  };
-
-  const handleClose = (event) => {
-    if (anchorRef.current && anchorRef.current.contains(event.target)) {
-      return;
-    }
-    setOpen(false);
-  };
-
-  function handleListKeyDown(event) {
-    if (event.key === "Tab") {
-      event.preventDefault();
-      setOpen(false);
-    }
-  }
-
-  // return focus to the button when we transitioned from !open -> open
-  const prevOpen = React.useRef(open);
-  React.useEffect(() => {
-    if (prevOpen.current === true && open === false) {
-      anchorRef.current.focus();
-    }
-
-    prevOpen.current = open;
-  }, [open]);
-
-  // Log Out Functionality
-  const { logOut } = useContext(LoginContext);
-  function handleLogOut(event) {
-    handleClose(event);
-    logOut();
-  }
-
   return (
     <StyledRoot>
       <div>
         <IconButton aria-label="home">
           <StyledHomeIcon />
         </IconButton>
-        <IconButton
-          ref={anchorRef}
-          aria-controls={open ? "menu-list-grow" : undefined}
-          aria-haspopup="true"
-          aria-label="menu"
-          onClick={handleToggle}
-        >
-          <SyledPersonIcon />
-        </IconButton>
+        <AccountMenu />
         <IconButton color="secondary">
           <SyledAddBoxIcon />
         </IconButton>
-        <Popper
-          open={open}
-          anchorEl={anchorRef.current}
-          role={undefined}
-          transition
-          disablePortal
-        >
-          {({ TransitionProps, placement }) => (
-            <Grow
-              {...TransitionProps}
-              style={{
-                transformOrigin:
-                  placement === "bottom" ? "center top" : "center bottom",
-              }}
-            >
-              <Paper>
-                <ClickAwayListener onClickAway={handleClose}>
-                  <MenuList
-                    autoFocusItem={open}
-                    id="menu-list-grow"
-                    onKeyDown={handleListKeyDown}
-                  >
-                    <MenuItem onClick={handleClose}>Profile</MenuItem>
-                    <MenuItem onClick={handleClose}>My account</MenuItem>
-                    <MenuItem onClick={handleLogOut}>Logout</MenuItem>
-                  </MenuList>
-                </ClickAwayListener>
-              </Paper>
-            </Grow>
-          )}
-        </Popper>
       </div>
     </StyledRoot>
   );
@@ -122,9 +36,6 @@ const sharedIconStyles = css`
 `;
 
 const StyledHomeIcon = styled(HomeIcon)`
-  ${sharedIconStyles}
-`;
-const SyledPersonIcon = styled(PersonIcon)`
   ${sharedIconStyles}
 `;
 
