b8a64c5dc23d67976c51e86da9173d999fd953ae
nbry <nwbryant@umich.edu>
2021-01-15 13:14:16 -0800

    Get rid of redundant context for NewBlogPost

--- src/components/NewBlogPostContext.js
+++
@@ -1,8 +0,0 @@
-// Although it seems weird, NewBlogPost context will be tied to the Header.
-// The header only appears when the user is logged in.
-// Also, the user should be able to make a new BlogPost regardless of where
-// they are on the site.
-
-import { createContext } from "react";
-
-export default createContext();
