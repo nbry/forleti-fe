25beeda5e1be22860d1a830943e10da33a1552f1
nbry <nwbryant@umich.edu>
2021-01-15 17:18:42 -0800

    Set logged in user to be stored in Login Context

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import LoginContext from "./components/LoginContext";
 import Routes from "./components/Routes";
 import Header from "./components/Header/Header";
-import "./App.css"
+import "./App.css";
+import BackendApi from "./helpers/BackendApi";
 
 function App() {
   // [LoginContext] CHECK IF USER IS LOGGED IN
@@ -10,21 +11,30 @@ function App() {
     localStorage.getItem("_token") !== null
   );
 
+  let [loggedInUser, setLoggedInUser] = useState({});
+
   const logOut = () => {
     localStorage.clear();
     setLoggedIn(false);
   };
 
   useEffect(() => {
-    if (localStorage.getItem("_token")) {
-      setLoggedIn(true);
+    async function getLoggedInUser() {
+      if (localStorage.getItem("_token")) {
+        let res = await BackendApi.getLoggedInUser();
+        setLoggedInUser(res.user);
+        setLoggedIn(true);
+      }
     }
+    getLoggedInUser();
   }, [loggedIn]);
   // [END LoginCOntext]
 
   return (
     <>
-      <LoginContext.Provider value={{ loggedIn, setLoggedIn, logOut }}>
+      <LoginContext.Provider
+        value={{ loggedIn, setLoggedIn, logOut, loggedInUser }}
+      >
         <Header />
         <Routes />
       </LoginContext.Provider>
