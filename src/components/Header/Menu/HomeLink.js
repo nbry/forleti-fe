3121c35be0c03d1b0c89c4c015daa6dca609680d
nbry <nwbryant@umich.edu>
2021-01-15 19:27:45 -0800

    Separate HomeLink from HeaderMenuList and make functional NavLink

--- src/components/Header/Menu/HomeLink.js
+++ src/components/Header/Menu/HomeLink.js
@@ -2,20 +2,16 @@ import React from "react";
 import { IconButton } from "@material-ui/core";
 import HomeIcon from "@material-ui/icons/Home";
 import styled, { css } from "styled-components";
-import AccountMenu from "./Menu/AccountMenu";
-import HomeLink from "./Menu/HomeLink";
-import NewBlogPost from "./NewBlogPost/NewBlogPost";
-import myColors from "../../static/colors";
+import { NavLink } from "react-router-dom";
+import myColors from "../../../static/colors";
 
 function HeaderMenuList() {
   return (
-    <StyledRoot>
-      <div>
-        <HomeLink />
-        <AccountMenu />
-        <NewBlogPost />
-      </div>
-    </StyledRoot>
+    <NavLink to="/home">
+      <IconButton aria-label="home">
+        <StyledHomeIcon />
+      </IconButton>
+    </NavLink>
   );
 }
 
@@ -34,8 +30,4 @@ const sharedIconStyles = css`
 
 const StyledHomeIcon = styled(HomeIcon)`
   ${sharedIconStyles}
-`;
-
-const StyledRoot = styled.div`
-  display: flex;
 `;
