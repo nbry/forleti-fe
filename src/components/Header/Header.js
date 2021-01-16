ff8247629ea64149680c162a640378d4e3b40d39
nbry <nwbryant@umich.edu>
2021-01-15 22:58:36 -0800

    Make a site logo a link to /home

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import HeaderMenuList from "./HeaderMenuList";
 import LoginContext from "../LoginContext";
 import myColors from "../../static/colors";
+import { NavLink } from "react-router-dom";
 
 function Header() {
   const { loggedIn } = useContext(LoginContext);
@@ -12,7 +13,9 @@ function Header() {
       {loggedIn ? (
         <StyledAppBar id="header" position="sticky" elevation={0}>
           <StyledToolbar>
-            <Title>Forleti</Title>
+            <Title>
+              <StyledNavLink to="/home">Forleti</StyledNavLink>
+            </Title>
             <HeaderMenuList />
           </StyledToolbar>
         </StyledAppBar>
@@ -34,6 +37,11 @@ const Title = styled.h1`
 const StyledAppBar = styled(AppBar)`
   background: ${myColors.background};
   border-bottom: 1px solid ${myColors.border};
+`;
+
+const StyledNavLink = styled(NavLink)`
+  text-decoration: none;
+  color: white;
 `;
 
 const StyledToolbar = styled(Toolbar)`
