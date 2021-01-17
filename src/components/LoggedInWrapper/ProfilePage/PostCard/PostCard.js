94dffd1cf14ddd3e5234d597bbb94074594169f3
nbry <nwbryant@umich.edu>
2021-01-16 17:40:39 -0800

    Create helper function to format date for PostCards

--- src/components/LoggedInWrapper/ProfilePage/PostCard/PostCard.js
+++ src/components/LoggedInWrapper/ProfilePage/PostCard/PostCard.js
@@ -2,12 +2,14 @@ import { Paper } from "@material-ui/core";
 import React from "react";
 import parse from "html-react-parser";
 import styled from "styled-components";
+import formatDate from "../../../../helpers/functions/formatDate";
 
 function PostCard({ post }) {
   return (
     <StyledPaper elevation={5}>
       <PostCardTitle>{post.title}</PostCardTitle>
       <div className="postContent">{parse(post.content)}</div>
+      <CreatedDate>{formatDate(post.created)}</CreatedDate>
     </StyledPaper>
   );
 }
@@ -20,7 +22,10 @@ const PostCardTitle = styled.h3`
   font-family: "Raleway";
   font-size: 1.6em;
   margin: 0px;
-  text-align: center;
+`;
+
+const CreatedDate = styled.small`
+  color: gray;
 `;
 
 const StyledPaper = styled(Paper)`
---
+++ src/helpers/functions/formatDate.js
@@ -0,0 +1,18 @@
+function formatDate(dateTimeFromPython) {
+  // prettier-ignore
+  const months = ["Jan", "Feb", "Mar", "Apr", "May",
+    "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
+  ];
+  const dateTime = new Date(dateTimeFromPython);
+  const hour = dateTime.getHours() !== 0 ? dateTime.getHours() % 12 : 12;
+  const meridiem = dateTime.getHours() < 12 ? "AM" : "PM";
+  const minute = dateTime.getMinutes();
+  const month = months[dateTime.getMonth()];
+  const date = dateTime.getDate();
+  const year = dateTime.getFullYear();
+
+  const formattedDate = `${hour}:${minute} ${meridiem} - ${month} ${date}, ${year}`;
+  return formattedDate;
+}
+
+export default formatDate;
