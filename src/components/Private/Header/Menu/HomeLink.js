3b53119bc0f5a51a2aa896b199572441f4cf6320
nbry <nwbryant@umich.edu>
2021-01-17 13:57:44 -0800

    Clean up imports and naming in HomeLink

--- src/components/Private/Header/Menu/HomeLink.js
+++ src/components/Private/Header/Menu/HomeLink.js
@@ -1,11 +1,11 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import { IconButton } from "@material-ui/core";
 import HomeIcon from "@material-ui/icons/Home";
 import styled, { css } from "styled-components";
-import { NavLink } from "react-router-dom";
 import myColors from "utils/static/colors";
 
-function HeaderMenuList() {
+function HomeLink() {
   return (
     <NavLink to="/home">
       <IconButton aria-label="home">
@@ -15,10 +15,9 @@ function HeaderMenuList() {
   );
 }
 
-export default HeaderMenuList;
+export default HomeLink;
 
 // STYLES:
-
 const sharedIconStyles = css`
   font-size: 1.5rem;
   color: white;
