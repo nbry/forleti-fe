593978fe69ed2433a0d43445fdbed6253586b63c
nbry <nwbryant@umich.edu>
2021-01-17 10:34:18 -0800

    Add rel attribute to a tag in PreReleaseNotice

--- src/static/PreReleaseNotice.js
+++ src/static/PreReleaseNotice.js
@@ -6,7 +6,10 @@ function PreReleaseNotice() {
       NOTICE: Forleti is in pre-release. We are still tinkering with core
       components and undergoing requirements analysis. Any accounts made prior
       to official release may be lost. Follow us on{" "}
-      <a href="https://twitter.com/forleti" target="_blank">
+      <a
+        href="https://twitter.com/forleti"
+        target="_blank"
+        rel="noopener noreferrer">
         Twitter
       </a>{" "}
       to receive updates on development!
