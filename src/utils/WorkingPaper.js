81ca9bddc75fb0dce083caec73ca9dddffc91282
nbry <nwbryant@umich.edu>
2021-01-17 17:47:14 -0800

    Fix bug with WorkingPaper maintaining a fixed height

--- src/utils/WorkingPaper.js
+++ src/utils/WorkingPaper.js
@@ -5,7 +5,7 @@ import styled from "styled-components";
 const WorkingPaper = styled(Paper)`
   margin: 20px;
   padding: 50px;
-  height: 91vh;
+  min-height: 91vh;
 `;
 
 export default WorkingPaper;
