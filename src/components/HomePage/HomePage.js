9079c36feb15399739c1d1068898b272ba49244e
nbry <nwbryant@umich.edu>
2021-01-20 23:32:58 -0800

    Add joke on home page

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,4 +1,4 @@
-import { Grid, Typography } from "@material-ui/core";
+import { Box, Grid, Typography } from "@material-ui/core";
 import React, { useContext, useEffect } from "react";
 import styled from "styled-components";
 import BackgroundWrapper from "components/BackgroundWrapper";
@@ -13,20 +13,13 @@ function HomePage() {
 
   return (
     <BackgroundWrapper user={loggedInUser} setContent="home">
-
-        <Grid container direction="column">
-          <Title variant="h2">Welcome to Forleti</Title>
-        </Grid>
-
+      <Grid container direction="column" alignItems="center">
+        <Title variant="h2">Welcome to Forleti</Title>
+        <Box mt={5}>
+          <StyledImage src={process.env.PUBLIC_URL + "/assets/if-i-made-one.png"} />
+        </Box>
+      </Grid>
     </BackgroundWrapper>
-
-    // <Fade in={pageLoaded} {...(pageLoaded ? { timeout: 500 } : {})}>
-    //   <WorkingPaper elevation={10}>
-    //     <Grid container direction="column">
-    //       <Title variant="h2">Welcome to Forleti</Title>
-    //     </Grid>
-    //   </WorkingPaper>
-    // </Fade>
   );
 }
 
@@ -35,4 +28,9 @@ export default HomePage;
 //STYLES:
 const Title = styled(Typography)`
   font-family: "Raleway";
+`;
+
+const StyledImage = styled.img`
+  max-width: 100%;
+  height: auto;
 `;
