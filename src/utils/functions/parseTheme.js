c40ac8202fcfbd151c27d4fa1f307ce8a4450822
nbry <nwbryant@umich.edu>
2021-02-01 12:04:30 -0800

    Add description to parseTheme function

--- src/utils/functions/parseTheme.js
+++ src/utils/functions/parseTheme.js
@@ -1,3 +1,10 @@
+// This function is used to parse a theme based on an integer provided.
+// Themes are stored in the database as integeres.  Themes are dictated by
+// /utils/static/profileThemes.js.
+// If a intenger is not provided, the default theme is returned.
+// Use this function when altering the visuals of the BackgroundWrapper component,
+// which is used for the Profile, Settings, and Home page of Forleti
+
 import myColors from "utils/static/colors";
 import profileThemes from "utils/static/profileThemes";
 
